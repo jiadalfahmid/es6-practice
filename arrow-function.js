e41bc9c2e62d7cf151d4fa82f5c98240a96b7007
jiadalfahmid <jiadalfahmid@gmail.com>
2024-10-02 14:42:46 +0600

    Arrow Function Explained

--- arrow-function.js
+++ arrow-function.js
@@ -1,1 +1,19 @@
-console.log('arrowFunction')
+// normal function
+
+function aFunction() {
+  console.log("simple function");
+}
+aFunction();
+
+// We can write an arrow function like a variable
+// variable-type functionName = (parameters) => {function body};
+// If an arrow written in one line then it returns by itself.
+const bFunction = () => "arrow function";
+console.log(bFunction());
+
+// multiline arrow
+// we need to return manually on this case.
+const cFunction = () => {
+  return console.log("multiline arrow");
+};
+cFunction();
